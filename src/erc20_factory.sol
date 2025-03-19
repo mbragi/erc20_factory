b96bfd79c5287de7338ff591ea13815bee959aa8
mbragi <spontaneousmanji@gmail.com>
2025-03-20 00:55:49 +0100

    feat: implement ERC20Factory with contract creation and tracking functionality

--- src/erc20_factory.sol
+++ src/erc20_factory.sol
@@ -4,9 +4,39 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Factory {
+    address owner;
+    mapping (unit => address) contractAddress;
+
+    uint256 public contractCount;
+
+    constructor() {
+        owner = msg.sender;
+        contractCount = 0;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
     function createERC20(string memory name, string memory symbol, uint8 decimals, uint256 totalSupply, address owner) public returns (address) {
         ERC20 token = new ERC20(name, symbol, decimals, totalSupply);
         token.transferOwnership(owner);
+        contractCount++;
+        contractAddress[contractCount] = address(token);
         return address(token);
     }
+
+    function getContractAddress(uint256 index) public view returns (address) {
+        return contractAddress[index];
+    }
+
+    function getAllContractAddress() public view returns (address[] memory) {
+        address[] memory addresses = new address[](contractCount);
+        for (uint i = 0; i < contractCount; i++) {
+            addresses[i] = contractAddress[i + 1];
+        }
+        return addresses;
+    }
+
 } 
