1ca23f2f1e88d16a691c365dc22538e8c546a845
mbragi <spontaneousmanji@gmail.com>
2025-03-20 12:46:16 +0100

    feat: add MintableERC20 contract and enhance ERC20Factory with token creation and tracking

--- src/erc20_factory.sol
+++ src/erc20_factory.sol
@@ -1,12 +1,27 @@
 // SPDX-License-identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract MintableERC20 is ERC20, Ownable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
 
 contract ERC20Factory {
     address owner;
-    mapping(unit => address) contractAddress;
-
+    
+    struct TokenInfo {
+        address tokenAddress;
+        address creator;
+    }
+    
+    mapping(uint => TokenInfo) public tokenInfo;
     uint256 public contractCount;
 
     constructor() {
@@ -19,25 +34,33 @@ contract ERC20Factory {
         _;
     }
 
-    function createERC20(string memory name, string memory symbol, uint8 decimals, uint256 totalSupply, address owner)
+    function createERC20(string memory name, string memory symbol, uint256 totalSupply, address creator)
         public
         returns (address)
     {
-        ERC20 token = new ERC20(name, symbol, decimals, totalSupply);
-        token.transferOwnership(owner);
+        MintableERC20 token = new MintableERC20(name, symbol);
+        token.transferOwnership(creator);
+        if (totalSupply > 0) {
+            token.mint(creator, totalSupply);
+        }
+        
         contractCount++;
-        contractAddress[contractCount] = address(token);
+        tokenInfo[contractCount] = TokenInfo(address(token), creator);
         return address(token);
     }
 
     function getContractAddress(uint256 index) public view returns (address) {
-        return contractAddress[index];
+        return tokenInfo[index].tokenAddress;
+    }
+
+    function getTokenCreator(uint256 index) public view returns (address) {
+        return tokenInfo[index].creator;
     }
 
     function getAllContractAddress() public view onlyOwner returns (address[] memory) {
         address[] memory addresses = new address[](contractCount);
         for (uint256 i = 0; i < contractCount; i++) {
-            addresses[i] = contractAddress[i + 1];
+            addresses[i] = tokenInfo[i + 1].tokenAddress;
         }
         return addresses;
     }
